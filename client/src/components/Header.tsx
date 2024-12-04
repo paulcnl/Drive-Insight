3b89c3906575af699c53c9da354980041e29bea3
malachai <malachaimorrison@gmail.com>
2024-12-04 16:07:54 +0100

    for comrade

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -4,30 +4,35 @@ function Header() {
   return (
     <header>
       <div className="header-mobile">
-        <img
-          src="./src/assets/images/menu-3-fill.png"
-          alt="menu-burger"
-          className="menu-burger"
-        />
-        <div>
+        <div className="header-logo">
           <h2>Insert Logo here</h2>
+          <Link to="/">
+            <img
+              src="./src/assets/images/menu-3-fill.svg"
+              alt="menu-burger"
+              className="menu-burger"
+            />
+          </Link>
+        </div>
+        <div className="navbar-mobile">
           <Link to="/compte">
-            <button type="button" className="navbar-mobile">
+            <button type="button" className="navbar-button-mobile">
               <h2>Compte</h2>
             </button>
           </Link>
           <Link to="/info">
-            <button type="button" className="navbar-mobile">
+            <button type="button" className="navbar-button-mobile">
               <h2>Infos env.</h2>
             </button>
           </Link>
           <Link to="/contact">
-            <button type="button" className="navbar-mobile">
+            <button type="button" className="navbar-button-mobile">
               <h2>Contact</h2>
             </button>
           </Link>
         </div>
       </div>
+
       <div className="nav">
         <Link to="/contact">
           <button type="button" className="button-desktop">
