1db724848aca3e4d9f4f3f8550a35cf910f888b2
paulcnl <paul.canarelli.2504@gmail.com>
2024-12-04 11:51:15 +0100

    layout-home-accueil

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,6 +7,7 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 // Import the main app component
 import App from "./App";
+import Home from "./pages/home";
 
 // Import additional components for new routes
 // Try creating these components in the "pages" folder
@@ -25,6 +26,7 @@ const router = createBrowserRouter([
     children: [
       {
         index: true,
+        element: <Home />,
       },
     ],
   },
