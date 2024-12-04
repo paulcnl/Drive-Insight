a0070ac7857b1beaefa75f050f00c96eac5db1b7
malachai <malachaimorrison@gmail.com>
2024-12-04 12:23:17 +0100

    begin for nav

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,19 @@
+import "./Header.css";
+function Header() {
+  return (
+    <header>
+      <h2>Insert Logo here</h2>
+      <div className="navbar">
+        <h2>Compte</h2>
+      </div>
+      <div className="navbar">
+        <h2>Infos env.</h2>
+      </div>
+      <div className="navbar">
+        <h2>Contact</h2>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
