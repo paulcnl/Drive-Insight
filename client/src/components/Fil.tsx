dde741abc31400f1c3dfb330e7f7fbe20ab737d9
salviamak <salvia.mak@gmail.com>
2024-12-05 10:37:22 +0100

    add nav-mobile

--- client/src/components/Fil.tsx
+++ client/src/components/Fil.tsx
@@ -1,14 +1,32 @@
-import car from "../assets/images/roadster.svg";
-import param from "../assets/images/settings.svg";
-import volant from "../assets/images/steering.svg";
-import men from "../assets/images/user.svg";
+import car from "../assets/images/roadster.png";
+import volant from "../assets/images/sateering.png";
+import param from "../assets/images/settings.png";
+import men from "../assets/images/user.png";
 const FilDAriane = () => {
   return (
-    <div className="box-ariane">
-      <img src={car} alt="" />
-      <img src={volant} alt="" />
-      <img src={param} alt="" />
-      <img src={men} alt="" />
+    <div className="container-box">
+      <ul className="fil-ariane">
+        <li className="fil-ariane__item">
+          <span className="fil-ariane__inner">
+            <img src={car} alt="" />
+          </span>
+        </li>
+        <li className="fil-ariane__item">
+          <span className="fil-ariane__inner">
+            <img src={volant} alt="" />
+          </span>
+        </li>
+        <li className="fil-ariane__item">
+          <span className="fil-ariane__inner">
+            <img src={param} alt="" />
+          </span>
+        </li>
+        <li className="fil-ariane__item">
+          <span className="fil-ariane__inner">
+            <img src={men} alt="" />
+          </span>
+        </li>
+      </ul>
     </div>
   );
 };
--- client/src/components/Matricule.tsx
+++ client/src/components/Matricule.tsx
@@ -1,7 +1,39 @@
+import add_car from "../assets/images/add-line.png";
+
 export default function Matricule() {
   return (
-    <div>
-      <input type="text" />
+    <div className="box-comparator">
+      <h2>Mon vehicule</h2>
+      <div className="comparator_child">
+        <h3>Immatriculation</h3>
+        <div className="add_car">
+          <img className="add_item" src={add_car} alt="" />
+          <input
+            type="text"
+            placeholder="Entrez votre plaque d'immatriculation"
+          />
+        </div>
+      </div>
+      <button type="submit">Valider</button>
+      <div className="separator">
+        <hr />
+        ou
+        <hr />
+      </div>
+      <div className="registred_info">
+        <h3>informations du vehicule</h3>
+        <div className="car_info">
+          <input type="text" placeholder="Marque" />
+          <input type="text" placeholder="Modele" />
+          <input type="text" placeholder="Kilometrage" />
+          <input type="text" placeholder="Puissance" />
+          <input type="text" placeholder="Cylindre" />
+          <input type="text" placeholder="Année" />
+          <input type="text" placeholder="energie" />
+          <input type="text" placeholder="" />
+        </div>
+        <button type="button">Enregistrer</button>
+      </div>
     </div>
   );
 }
