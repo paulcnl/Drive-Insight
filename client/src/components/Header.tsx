039145dd165fde768e60cc2866be23f563ebc671
malachai <malachaimorrison@gmail.com>
2024-12-09 09:50:45 +0100

    first design for navbar

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -11,67 +11,26 @@ function Header() {
           <h2>Insert Logo here</h2>
           <button
             type="button"
+            className="menu-burger"
             onClick={() => {
               setIsClicked(!isClicked);
             }}
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="150"
-              height="150"
-              viewBox="0 0 200 200"
-              className="menu-icon"
-            >
-              <title>Menu Icon</title>
-              <g stroke-width="6.5" stroke-linecap="round">
-                <path
-                  d="M72 82.286h28.75"
-                  fill="#009100"
-                  fill-rule="evenodd"
-                  stroke="#fff"
-                />
-
-                <path
-                  d="M100.75 103.714l72.482-.143c.043 39.398-32.284 71.434-72.16 71.434-39.878 0-72.204-32.036-72.204-71.554"
-                  fill="none"
-                  stroke="#fff"
-                />
-                <path
-                  d="M72 125.143h28.75"
-                  fill="#009100"
-                  fill-rule="evenodd"
-                  stroke="#fff"
-                />
-                <path
-                  d="M100.75 103.714l-71.908-.143c.026-39.638 32.352-71.674 72.23-71.674 39.876 0 72.203 32.036 72.203 71.554"
-                  fill="none"
-                  stroke="#fff"
-                />
-                <path
-                  d="M100.75 82.286h28.75"
-                  fill="#009100"
-                  fill-rule="evenodd"
-                  stroke="#fff"
-                />
-                <path
-                  d="M100.75 125.143h28.75"
-                  fill="#009100"
-                  fill-rule="evenodd"
-                  stroke="#fff"
-                />
-              </g>
-            </svg>
+            <img
+              src="./src/assets/images/menu-3-line (2).png"
+              alt="menu-burger"
+            />
           </button>
         </div>
         <div className={isClicked ? "navbar-mobile" : "none"}>
           <Link to="/compte" className="navbar-button-mobile">
-            Compte
+            <span>Compte</span>
           </Link>
           <Link to="/info" className="navbar-button-mobile">
-            Infos env.
+            <span>Infos env.</span>
           </Link>
           <Link to="/contact" className="navbar-button-mobile">
-            Contact
+            <span>Contact</span>
           </Link>
         </div>
       </div>
