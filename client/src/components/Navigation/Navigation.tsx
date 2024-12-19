9a4821a950ad7578b2c01542371a4cbd70393dc7
malachai <malachaimorrison@gmail.com>
2024-12-19 15:36:42 +0100

    for presentation tmr

--- client/src/components/Navigation/Navigation.tsx
+++ client/src/components/Navigation/Navigation.tsx
@@ -2,39 +2,43 @@ import { Link } from "react-router-dom";
 import "./Navigation.css";
 
 function Navigation() {
+  function handleClick(_noshow: boolean) {
+    throw new Error("Function not implemented.");
+  }
+
   return (
     <div className="nav-container">
       <Link
-        // onClick={() => {
-        //   handleClick(!isClicked);
-        // }}
+        onClick={() => {
+          handleClick(false);
+        }}
         to="/contact"
         className="navbar-button"
       >
         Contact
       </Link>
       <Link
-        // onClick={() => {
-        //   handleClick(!isClicked);
-        // }}
+        onClick={() => {
+          handleClick(false);
+        }}
         to="/info"
         className="navbar-button"
       >
         Info
       </Link>
       <Link
-        // onClick={() => {
-        //   handleClick(!isClicked);
-        // }}
+        onClick={() => {
+          handleClick(false);
+        }}
         to="/comparer"
         className="navbar-button"
       >
         Comparer
       </Link>
       <Link
-        // onClick={() => {
-        //   handleClick(!isClicked);
-        // }}
+        onClick={() => {
+          handleClick(false);
+        }}
         to="/compte"
         className="navbar-button"
       >
