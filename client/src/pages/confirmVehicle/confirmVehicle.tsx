bd906a15b7b4f49022c92353f9f502d63897c5f0
paulcnl <paul.canarelli.2504@gmail.com>
2024-12-19 16:43:10 +0100

    etape-2

--- client/src/pages/confirmVehicle/confirmVehicle.tsx
+++ client/src/pages/confirmVehicle/confirmVehicle.tsx
@@ -1,9 +1,19 @@
 import "./confirmVehicle.css";
+import FilAriane from "../../components/FilAriane";
 
 function Confirm() {
   return (
     <>
+      <FilAriane />
       <h1>confirm</h1>
+      <div className="confirm_cards">
+        <h2>Mon vehicule 1</h2>
+        <h2>Mon vehicule 2</h2>
+        <h2>Mon verhicule 3</h2>
+      </div>
+      <div className="btn_confirm_cards">
+        <button type="button">Valider</button>
+      </div>
     </>
   );
 }
--- client/src/pages/habits/Habits.tsx
+++ client/src/pages/habits/Habits.tsx
@@ -1,7 +1,74 @@
 import "./Habits.css";
 
 function Habits() {
-  return <></>;
+  return (
+    <>
+      <div className="habits">
+        <h2>Mes habitudes</h2>
+        <div className="habits-container">
+          <div className="input-container">
+            <label htmlFor="distance"> distance</label>
+            <input
+              type="text"
+              name="distance"
+              id="distance"
+              className="distance-input"
+            />
+          </div>
+          <p>km</p>
+          <div className="input-container">
+            <label htmlFor="frequence"> frequence</label>
+            <input
+              type="text"
+              name="frequence"
+              id="frequence"
+              className="frequence-input"
+            />
+          </div>
+          <div className="input-container">
+            <label htmlFor="frequence"> </label>
+            <select name="option" id="habits-option" className="option">
+              <option value="jour">jours </option>
+              <option value="mois">mois </option>
+              <option value="années">années </option>
+            </select>
+          </div>
+        </div>
+        <div className="habits-container">
+          <div className="input-container">
+            <label htmlFor="distance"> distance</label>
+            <input
+              type="text"
+              name="distance"
+              id="distance"
+              className="distance-input"
+            />
+          </div>
+          <p>km</p>
+          <div className="input-container">
+            <label htmlFor="frequence"> frequence</label>
+            <input
+              type="text"
+              name="frequence"
+              id="frequence"
+              className="frequence-input"
+            />
+          </div>
+          <div className="input-container">
+            <label htmlFor="frequence"> </label>
+            <select name="option" id="habits-option" className="option">
+              <option value="jour">jours </option>
+              <option value="mois">mois </option>
+              <option value="années">années </option>
+            </select>
+          </div>
+        </div>
+        <div className="button-cont">
+          <button type="button">valider</button>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Habits;
