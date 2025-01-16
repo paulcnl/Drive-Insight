8569c2a0e51d3045e39592b95574e7c8c0ca2ad9
jottiana <11.paradise.avenue@gmail.com>
2025-01-16 22:59:51 +0100

    added a back to home link + modified the navigation bar to meet accessibility criteria + refactored the CSS + restored package-lock.json to resolve pre-commit hook errors

--- client/src/components/Navigation/Navigation.tsx
+++ client/src/components/Navigation/Navigation.tsx
@@ -3,20 +3,46 @@ import "./Navigation.css";
 
 function Navigation({ handleClick }: { handleClick: () => void }) {
   return (
-    <div className="nav-container">
-      <Link onClick={handleClick} to="/contact" className="navbar-button">
+    <nav className="nav-container" aria-label="Navigation principale">
+      <Link
+        onClick={handleClick}
+        to="/contact"
+        className="nav-link"
+        aria-current={
+          window.location.pathname === "/contact" ? "page" : undefined
+        }
+      >
         Contact
       </Link>
-      <Link onClick={handleClick} to="/info" className="navbar-button">
+      <Link
+        onClick={handleClick}
+        to="/info"
+        className="nav-link"
+        aria-current={window.location.pathname === "/info" ? "page" : undefined}
+      >
         Info
       </Link>
-      <Link onClick={handleClick} to="/comparer" className="navbar-button">
+      <Link
+        onClick={handleClick}
+        to="/comparer"
+        className="nav-link"
+        aria-current={
+          window.location.pathname === "/comparer" ? "page" : undefined
+        }
+      >
         Comparer
       </Link>
-      <Link onClick={handleClick} to="/compte" className="navbar-button">
+      <Link
+        onClick={handleClick}
+        to="/compte"
+        className="nav-link"
+        aria-current={
+          window.location.pathname === "/compte" ? "page" : undefined
+        }
+      >
         Compte
       </Link>
-    </div>
+    </nav>
   );
 }
 
